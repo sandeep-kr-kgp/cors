13456e0b16273a9305011a61d09a0f20d3c3bd63
sandeep-kr-kgp <sandeepkr.kgp@gmail.com>
2024-03-14 19:30:19 +0530

    commit

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ myWorker.onmessage = function (e) {
   app.innerHTML = `<pre>${JSON.stringify(JSON.parse(e.data), null, 4)}</pre>`;
   console.log(e.data);
 };
+const response = fetch(
+  `https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/rates_of_exchange?fields=country_currency_desc,exchange_rate,%20record_date&filter=record_date:gte:2015-01-01`
+);
+console.log('response in main thread', response);
 app.innerHTML = `
   Hello
 `;
